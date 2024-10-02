9a7f1fed6e9f159ff247bc5cc108acfa1f7e5a53
R-YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY-N <Aba7sain@gmail.com>
2024-10-02 17:36:38 +0300

    QueueStackTester split test1

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -3,14 +3,20 @@ package com.example.project;
 public class QueueStackTester {
     public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
         // Write the recursive static method split that splits a queue of n elements into two
         // queues. The elements with odd orders (i.e. 1st, 3rd, 5th ...) should be put in the
         // first queue and elements with even orders (i.e. 2nd, 4th, 6th ...) should be put in
         // the second queue. The original queue should remain unchanged at the end of the
         // method.
         // Example. Given the queue (A, B, C, D, E), split results in oq (A, C, E), and eq(B, D).
+    	for(int i = 0; i < q.length(); i++) {
+    		if(i % 2 == 0)
+    			eq.enqueue(q.serve());
+    		else
+    			oq.enqueue(q.serve());
+    	}
     }
+    
     public static <T> void remove(LinkedPQ<T> pq, int p)
     {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -19,6 +25,7 @@ public class QueueStackTester {
         // Example. Given pq: [A, 10], [D, 8], [B, 5], [E, 3], [C, 2] remove(pq, 5) results in
         // pq: [A, 10], [D, 8], [B, 5].
     }
+    
     public static <T> boolean search(Stack<T> st, T e)
     {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -27,5 +34,44 @@ public class QueueStackTester {
         // the method. You are not allowed to use any auxiliary data structures.
         // Example. Given the stack st (top-to-bottom): 5, 7, 5, 3, 2. search(st, 3) returns
         // true while search(st, 1) returns false.
+    }
+    
+    public static boolean isOrdered(Queue<Integer> q) {
+    	boolean flag = true;
+    	int e1 = q.serve();
+    	q.enqueue(e1);
+    	for (int i = 1; i < q.length(); i++) {
+    		int e2 = q.serve();
+    		q.enqueue(e2);
+    		if(e1 > e2)
+    			flag = false;
+    		e1 = e2;
+    	}
+    	return flag;
+    }
+    
+    public static int stackSum(Stack<Integer> st) {
+    	if(st.empty())
+    		return 0;
+    	int x = st.pop();
+    	int y = stackSum(st);
+    	st.push(x);
+    	return x + y;
+    }
+    
+    public static void main(String args[]) {
+    	ArrayStack<Integer> as = new ArrayStack<Integer>(10);
+    	LinkedStack<Integer> ls = new LinkedStack<Integer>();
+    	as.push(1);
+    	as.push(2);
+    	as.push(3);
+    	as.push(4);
+    	System.out.println(stackSum(as));
+    	ls.push(1);
+    	ls.push(2);
+    	ls.push(3);
+    	ls.push(4);
+    	ls.push(5);
+    	System.out.println(stackSum(ls));
     }
 }
